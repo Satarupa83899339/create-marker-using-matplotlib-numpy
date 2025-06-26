b275e61a3a74bccc11e660759ce3f420af0275cc
Satarupa Kundu <satarupakundu16@gmail.com>
2025-06-26 17:45:11 +0530

    Update star marker.py

--- star marker.py
+++ star marker.py
@@ -5,3 +5,28 @@ import numpy as np
 ypoints = np.array([3, 12, 8, 25])
 plt.plot(ypoints, marker = '*')
 plt.show()
+
+''' 'o'	Circle	
+'*'	Star	
+'.'	Point	
+','	Pixel	
+'x'	X	
+'X'	X (filled)	
+'+'	Plus	
+'P'	Plus (filled)	
+'s'	Square	
+'D'	Diamond	
+'d'	Diamond (thin)	
+'p'	Pentagon	
+'H'	Hexagon	
+'h'	Hexagon	
+'v'	Triangle Down	
+'^'	Triangle Up	
+'<'	Triangle Left	
+'>'	Triangle Right	
+'1'	Tri Down	
+'2'	Tri Up	
+'3'	Tri Left	
+'4'	Tri Right	
+'|'	Vline	
+'_'	Hline '''
